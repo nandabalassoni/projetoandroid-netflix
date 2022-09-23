b75d6b5560a25541ac3bb51dab17506c6d04bdd1
nandabalassoni <mariafernandabalassoni@hotmail.com>
2022-09-23 14:38:02 -0300

    adicionando imagem e criando tela que mostra o filme

--- app/src/main/java/com/example/projetoandroid_netflix/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/projetoandroid_netflix/ui/activity/MainActivity.kt
@@ -2,9 +2,11 @@ package com.example.projetoandroid_netflix.ui.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.projetoandroid_netflix.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_filmes)
     }
 }
